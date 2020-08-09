f8aebf1bf7daabe67f03c0936fcd12f6b785980e
Apoorva Srivastava <asrivastava@WellDyneRx.com>
2020-08-09 17:40:39 +0530

    changes

---
+++ client/build/precache-manifest.d1bea61bcb417e55308a17b1d7d109c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49654bf5b28b505626369b6ae5752649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8c13e3bb88c2f1b8c56",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "8e54adecdfd49d583ba3",
+    "url": "/static/js/2.479223bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.479223bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c13e3bb88c2f1b8c56",
+    "url": "/static/js/main.92252aa7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
